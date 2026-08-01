0a2f6edef5f5d5ca09f786a69ad0909e01986d3c
Mayhaul <mehulv2k6@gmail.com>
2026-08-01 21:39:46 +0530

    added headers and footers

--- hairbnb-backend/src/app.js
+++ hairbnb-backend/src/app.js
@@ -2,12 +2,15 @@ import express from 'express';
 const app = express();
 import path from "path";
 import { fileURLToPath } from "url";
+import ejsMate from "ejs-mate";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'));
+
+app.engine("ejs", ejsMate);
 
 import routes from './routes/index.js'
 
