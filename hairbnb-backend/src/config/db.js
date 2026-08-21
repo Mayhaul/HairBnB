af65d5f769347fb1755d36d3490429b3c66b629f
Mayhaul <mehulv2k6@gmail.com>
2026-08-21 16:27:23 +0530

    fix(db): update MongoDB connection to use environment variable for URI

--- hairbnb-backend/src/config/db.js
+++ hairbnb-backend/src/config/db.js
@@ -1,16 +1,19 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import dns from "dns";
+
 dotenv.config();
 
-const connectDB = async()=>{
-    try{
-        const conn = await mongoose.connect("mongodb://127.0.0.1:27017/hairbnb");
-        console.log(`connected`);
-    }catch(error){
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("connected");
+    } catch (error) {
         console.log(error);
         process.exit(1);
     }
-
-}
+};
 
 export default connectDB;
